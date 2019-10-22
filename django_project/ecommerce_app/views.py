7074e6f0708932da0e0c7c5fb2ec4521f9e94aff
Paulo Cesar <pcbillions4@gmail.com>
2019-10-21 23:01:08 -0300

    Add views to deal with returns and cancellations

--- django_project/ecommerce_app/views.py
+++ django_project/ecommerce_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect, \
     get_object_or_404, reverse
 from django.contrib import messages
+from django.conf import settings
+from decimal import Decimal
+from paypal.standard.forms import PayPalPaymentsForm
+from django.views.decorators.csrf import csrf_exempt
 from .models import Product, Order, LineItem
 from .forms import CartForm, CheckoutForm
 from . import cart
@@ -84,3 +88,32 @@ def checkout(request):
         form = CheckoutForm()
         return render(request, 'ecommerce_app/checkout.html', {'form': form})
 
+
+def process_payment(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    host = request.get_host()
+
+    paypal_dict = {
+        'business': settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': '%.2f' % order.total_cost().quantize(Decimal('.01')),
+        'item_name': 'Order {}'.format(order.id),
+        'invoice': str(order.id),
+        'currency_code': 'USD',
+        'notify_url': 'http://{}{}'.format(host, reverse('paypal-ipn')),
+        'return_url': 'http://{}{}'.format(host, reverse('payment_done')),
+        'cancel_return': 'http://{}{}'.format(host, reverse('payment_cancelled')),
+    }
+
+    form = PayPalPaymentsForm(initial=paypal_dict)
+    return render(request, 'ecommerce_app/process_payment.html', {'order': order, 'form': form})
+
+
+@csrf_exempt
+def payment_done(request):
+    return render(request, 'ecommerce_app/payment_done.html')
+
+
+@csrf_exempt
+def payment_canceled(request):
+    return render(request, 'ecommerce_app/payment_cancelled.html')
