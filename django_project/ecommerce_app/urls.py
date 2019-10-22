5af165831236a357ea7b360e14550e6ac38282bf
Paulo Cesar <pcbillions4@gmail.com>
2019-10-21 23:20:42 -0300

    Add URLs to deal with payments

--- django_project/ecommerce_app/urls.py
+++ django_project/ecommerce_app/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<int:product_id>/<slug:product_slug>/',
-        views.show_product, name='product_detail'),
+         views.show_product, name='product_detail'),
     path('cart/', views.show_cart, name='show_cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('process-payment/', views.process_payment, name='proccess_payment'),
+    path('payment-done/', views.payment_done, name='payment_done'),
+    path('payment-cancelled/', views.payment_canceled, name='payment_cancelled'),
 ]
